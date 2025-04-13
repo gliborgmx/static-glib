19ac830b4f6f4ede07e96d0020c8d602a3fbd1a4
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2025-04-13 17:56:32 +0200

    mastodon.py: corregir errores de pylint

--- mastodon.py
+++ mastodon.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
-import requests
-import uuid
-import os
-import sys
-import re
+"""Publish a Tooth given an MD file"""
+
 from typing import Optional
+
+import os
+import re
+import sys
+import uuid
+import requests
 
 MESSAGE = '¡Hemos publicado un nuevo texto!'
 BASE_BLOG_URL = 'https://glib.org.mx/'
@@ -17,9 +20,10 @@ META_RE = re.compile(r'^[ ]{0,3}(?P<key>[A-Za-z0-9_-]+)\s*=\s*"(?P<value>.*)"$')
 BEGIN_RE = re.compile(r'^\+{3}(\s.*)?')
 END_RE = re.compile(r'^(\+{3}|\.{3})(\s.*)?')
 
-ACCESS_TOKEN = None;
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 
 def publish(meta: dict) -> bool:
+    """HTTP post message to mastodon server with message to publish"""
     # https://docs.joinmastodon.org/methods/statuses/#headers
     headers: dict = {
         "Authorization": f"Bearer {ACCESS_TOKEN}",
@@ -33,7 +37,7 @@ def publish(meta: dict) -> bool:
             "visibility": "public",
             "language": "es",
         }
-    except KeyError as e:
+    except LookupError:
         print("meta doesn't contain title or slug")
         return False
 
@@ -41,38 +45,41 @@ def publish(meta: dict) -> bool:
         if meta['draft'] and meta['draft'][0] in ['true', 'True', 'yes', 'Yes', '1']:
             print(f"article {meta['slug'][0]} is a draft")
             return True
-    except:
+    except LookupError:
         pass
 
-    response = requests.post(BASE_URL, headers=headers, data=form_data)
-    if response.status_code == requests.codes.ok:
+    response = requests.post(BASE_URL, headers=headers, data=form_data, timeout=5)
+    if response.status_code == requests.codes.ok: # pylint: disable=no-member
         return True
 
     print(f"Request error: {response.text}")
     return False
 
 def get_meta(fname: str) -> Optional[dict]:
+    """Gets meta information of the post"""
     meta: dict[str, list[str]] = {}
-    with open(fname) as fn:
+    with open(fname, encoding="utf-8") as fn:
         line = next(fn).strip()
         if BEGIN_RE.match(line):
             while line:
                 line = next(fn).strip()
                 if END_RE.match(line):
                     return meta
-                else:
-                    m1 = META_RE.match(line)
-                    if m1:
-                        key = m1.group('key').lower().strip()
-                        value = m1.group('value').strip()
-                        try:
-                            meta[key].append(value)
-                        except KeyError:
-                            meta[key] = [value]
+
+                m1 = META_RE.match(line)
+                if m1:
+                    key = m1.group('key').lower().strip()
+                    value = m1.group('value').strip()
+                    try:
+                        meta[key].append(value)
+                    except LookupError:
+                        meta[key] = [value]
+
     print(f"No metada data found in {fname}")
     return None
 
 def process(fname: str) -> int:
+    """Gets article's meta and publish the link in mastodon"""
     meta = get_meta(fname)
     if meta and publish(meta):
         return 0
@@ -80,13 +87,7 @@ def process(fname: str) -> int:
     return -1
 
 def main() -> int:
-    try:
-        global ACCESS_TOKEN
-        ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
-    except KeyError as e:
-        print('No ACCESS_TOKEN env var found')
-        return -1
-
+    """Main function"""
     if ACCESS_TOKEN and len(sys.argv) == 2:
         return process(sys.argv[1])
     print('No access token or missing parameter')
