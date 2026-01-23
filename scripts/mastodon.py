bfc5e5ecb6d4d7c15483d7fa9c78c680c69d3fad
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2026-01-23 07:08:55 +0100

    scripts: mastodon.py: corregir pytlint

--- scripts/mastodon.py
+++ scripts/mastodon.py
@@ -14,13 +14,14 @@ MESSAGE = '¡Hemos publicado un nuevo texto!'
 BASE_BLOG_URL = 'https://glib.org.mx/'
 BASE_URL = 'https://floss.social/api/v1/statuses'
 
-URI='urn:ietf:wg:oauth:2.0:oob'
+URI = 'urn:ietf:wg:oauth:2.0:oob'
 
 META_RE = re.compile(r'^[ ]{0,3}(?P<key>[A-Za-z0-9_-]+)\s*=\s*"(?P<value>.*)"$')
 BEGIN_RE = re.compile(r'^\+{3}(\s.*)?')
 END_RE = re.compile(r'^(\+{3}|\.{3})(\s.*)?')
 
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+
 
 def publish(meta: dict) -> bool:
     """HTTP post message to mastodon server with message to publish"""
@@ -31,29 +32,37 @@ def publish(meta: dict) -> bool:
     }
 
     try:
-        # https://docs.joinmastodon.org/methods/statuses/#form-data-parameters
-        form_data: dict = {
-            "status": f"{MESSAGE}\n\n{meta['title'][0]}\n\n{BASE_BLOG_URL}{meta['slug'][0]}",
-            "visibility": "public",
-            "language": "es",
-        }
+        status = f"{MESSAGE}\n\n{meta['title'][0]}\n\n"\
+            f"{BASE_BLOG_URL}{meta['slug'][0]}"
     except LookupError:
         print("meta doesn't contain title or slug")
         return False
 
+    # https://docs.joinmastodon.org/methods/statuses/#form-data-parameters
+    form_data: dict = {
+        "status": status,
+        "visibility": "public",
+        "language": "es",
+    }
+
     try:
-        if meta['draft'] and meta['draft'][0] in ['true', 'True', 'yes', 'Yes', '1']:
+        if meta['draft'] and \
+           meta['draft'][0] in ['true', 'True', 'yes', 'Yes', '1']:
             print(f"article {meta['slug'][0]} is a draft")
             return True
     except LookupError:
         pass
 
-    response = requests.post(BASE_URL, headers=headers, data=form_data, timeout=5)
-    if response.status_code == requests.codes.ok: # pylint: disable=no-member
+    response = requests.post(BASE_URL,
+                             headers=headers,
+                             data=form_data,
+                             timeout=5)
+    if response.status_code == requests.codes.ok:  # pylint: disable=no-member
         return True
 
     print(f"Request error: {response.text}")
     return False
+
 
 def get_meta(fname: str) -> Optional[dict]:
     """Gets meta information of the post"""
@@ -78,6 +87,7 @@ def get_meta(fname: str) -> Optional[dict]:
     print(f"No metada data found in {fname}")
     return None
 
+
 def process(fname: str) -> int:
     """Gets article's meta and publish the link in mastodon"""
     meta = get_meta(fname)
@@ -86,12 +96,14 @@ def process(fname: str) -> int:
     print('Unknown error')
     return -1
 
+
 def main() -> int:
     """Main function"""
     if ACCESS_TOKEN and len(sys.argv) == 2:
         return process(sys.argv[1])
     print('No access token or missing parameter')
     return -1
+
 
 if __name__ == '__main__':
     sys.exit(main())
